f2f1d83b19a94f8206db2a7b4c886bac5193cee9
Allen Guo <guoguo12@gmail.com>
2014-12-17 15:08:47 -0800

    Add initial files from Berkeley Course Search 0.3.0

---
+++ server/proxy.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_SERVER['REQUEST_URI'])) {
+    $queryString = explode('?', $_SERVER['REQUEST_URI'])[1];
+    $path = 'https://apis-dev.berkeley.edu/cxf/asws/course?' . $queryString;
+    try {
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $path,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => array('app_id:4fc7196a',
+                                        'app_key:24296f1e62d48d91170d4d18ef4b43a5'),
+            CURLOPT_CONNECTTIMEOUT => 4,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false
+        ));
+        $output = curl_exec($ch);
+        echo $output;
+        curl_close($ch);
+    } catch (Exception $ex) {
+        echo $ex;
+    }
+} else {
+
+}
