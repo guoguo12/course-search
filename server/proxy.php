1b73f76225ec39badab5e1384cb8de4f34bdb7c4
Allen Guo <guoguo12@gmail.com>
2014-12-17 15:23:30 -0800

    API key shown in server code has been disabled

--- server/proxy.php
+++ server/proxy.php
@@ -7,6 +7,7 @@ if (isset($_SERVER['REQUEST_URI'])) {
         curl_setopt_array($ch, array(
             CURLOPT_URL => $path,
             CURLOPT_RETURNTRANSFER => true,
+            // The key shown has been disabled and is not the one used in prod
             CURLOPT_HTTPHEADER => array('app_id:4fc7196a',
                                         'app_key:24296f1e62d48d91170d4d18ef4b43a5'),
             CURLOPT_CONNECTTIMEOUT => 4,
